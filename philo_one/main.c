b84347336c6ae5ac74c0c7248f6f6d8363635494
U29maXEgIFNoaWRsb3dza2FxIA== U2hpZGxvd3NrYXEg <handrow@mi-h1.msk.21-school.ru>
2021-02-06 22:14:36 +0300

    add init print

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,7 +6,7 @@
 /*   By: handrow <handrow@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 18:26:27 by handrow           #+#    #+#             */
-/*   Updated: 2021/02/06 19:48:27 by handrow          ###   ########.fr       */
+/*   Updated: 2021/02/06 22:13:32 by handrow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@
 #include "utils.h"
 
 #define SLEEP_DELAY_US 100
+#define	MAX_PHILO_NUM 1000
+#define PHILO_WORKER_START_DELAY_MS 1000
 
 typedef int64_t		t_time_ms;
 
-enum e_setting_idx
+enum				e_setting_idx
 {
 	PHILO_NUM_ARG_IDX = 1,
 	PHILO_TTD_ARG_IDX = 2,
@@ -36,7 +38,7 @@ enum e_setting_idx
 	PHILO_ARG_LIMIT_MAX = 6
 };
 
-struct s_settings
+struct				s_settings
 {
 	int				philo_num;
 	int				meal_count;
@@ -45,7 +47,59 @@ struct s_settings
 	t_time_ms	 	time_to_die;
 };
 
+typedef struct		s_atomic_time_ms
+{
+	t_time_ms		value;
+	pthread_mutex_t	guard;
+}					t_atomic_time_ms;
+
+typedef struct		s_atomic_bool
+{
+	bool			value;
+	pthread_mutex_t	guard;
+}					t_atomic_bool;
+
 struct s_settings	g_settings;
+pthread_mutex_t		g_print_guard;
+t_time_ms			g_worker_start;
+pthread_mutex_t		g_fork_pool[MAX_PHILO_NUM];
+pthread_t			g_philo_pool[MAX_PHILO_NUM];
+t_atomic_time_ms	g_deadline_pool[MAX_PHILO_NUM];
+t_atomic_bool		g_exited_pool[MAX_PHILO_NUM];
+
+bool				get_atomic_bool(t_atomic_bool *atom)
+{
+	bool	val;
+
+	pthread_mutex_lock(&atom->guard);
+	val = atom->value;
+	pthread_mutex_unlock(&atom->guard);
+	return (val);
+}
+
+void				set_atomic_bool(t_atomic_bool *atom, bool value)
+{
+	pthread_mutex_lock(&atom->guard);
+	atom->value = value;
+	pthread_mutex_unlock(&atom->guard);
+}
+
+bool				get_atomic_time_ms(t_atomic_time_ms *atom)
+{
+	t_time_ms	val;
+
+	pthread_mutex_lock(&atom->guard);
+	val = atom->value;
+	pthread_mutex_unlock(&atom->guard);
+	return (val);
+}
+
+void				set_atomic_time_ms(t_atomic_time_ms *atom, bool value)
+{
+	pthread_mutex_lock(&atom->guard);
+	atom->value = value;
+	pthread_mutex_unlock(&atom->guard);
+}
 
 t_time_ms			get_current_time_ms(void)
 {
@@ -78,6 +132,76 @@ bool				fill_settings(struct s_settings *settings, int ac, char *av[])
 	return (true);
 }
 
+void				init_fork_pool(pthread_mutex_t fork_pool[], int num)
+{
+	int	i;
+
+	i = 0;
+	while (i < num)
+		pthread_mutex_init(&fork_pool[i++], NULL);
+}
+
+void				destroy_fork_pool(pthread_mutex_t fork_pool[], int num)
+{
+	int	i;
+
+	i = 0;
+	while (i < num)
+		pthread_mutex_destroy(&fork_pool[i++]);
+}
+
+void				init_status_pools(t_atomic_time_ms deadline_pool[],
+										t_atomic_bool exit_pool[], int num)
+{
+	int	i;
+
+	g_worker_start = get_current_time_ms() + PHILO_WORKER_START_DELAY_MS;
+	i = 0;
+	while (i < num)
+	{
+		deadline_pool[i].value = g_worker_start + g_settings.time_to_die;
+		pthread_mutex_init(&(deadline_pool[i++].guard), NULL);
+	}
+	i = 0;
+	while (i < num)
+	{
+		exit_pool[i].value = 0;
+		pthread_mutex_init(&(exit_pool[i++].guard), NULL);
+	}
+}
+
+
+
+void				destroy_status_pools(t_atomic_time_ms deadline_pool[],
+										t_atomic_bool exit_pool[], int num)
+{
+	int	i;
+
+	i = 0;
+	while (i < num)
+		pthread_mutex_destroy(&(deadline_pool[i++].guard));
+	i = 0;
+	while (i < num)
+		pthread_mutex_destroy(&(exit_pool[i++].guard));
+}
+
+void				init_atomic_print(pthread_mutex_t *print_guard)
+{
+	pthread_mutex_init(print_guard, NULL);
+}
+
+void				destroy_atomic_print(pthread_mutex_t *print_guard)
+{
+	pthread_mutex_destroy(print_guard);
+}
+
+void				print_atomic_message(int philo_idx, const char *msg)
+{
+	pthread_mutex_lock(&g_print_guard);
+	printf("%-6llu ms | %-3d | %s\n", get_current_time_ms(), philo_idx + 1, msg);
+	pthread_mutex_unlock(&g_print_guard);
+}
+
 int					main(int ac, char *av[])
 {
 	if (fill_settings(&g_settings, ac, av) == false)
@@ -85,6 +209,13 @@ int					main(int ac, char *av[])
 		printf("Invalid arguments\n");
 		return (EXIT_FAILURE);
 	}
-
+	init_atomic_print(&g_print_guard);
+	init_fork_pool(g_fork_pool, g_settings.philo_num);
+	init_status_pools(g_deadline_pool, g_exited_pool, g_settings.philo_num);
+	// LAUNCH WORKRES
+	// WATCH WORKERS
+	destroy_status_pools(g_deadline_pool, g_exited_pool, g_settings.philo_num);
+	destroy_fork_pool(g_fork_pool, g_settings.philo_num);
+	destroy_atomic_print(&g_print_guard);
 	return (EXIT_SUCCESS);
 }
